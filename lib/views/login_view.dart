285fb8fa6f4cd0d2200e70e98712af132b15bacf
Tushar Chavan <tusharrchavan014@gmail.com>
2022-10-16 15:50:28 +0530

    step-7:

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,100 +4,99 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
-    const LoginView({super.key});
+  const LoginView({super.key});
 
-    @override
-    State<LoginView> createState() => _LoginViewState();
+  @override
+  State<LoginView> createState() => _LoginViewState();
 }
 
 class _LoginViewState extends State<LoginView> {
-    late final TextEditingController _email;
-    late final TextEditingController _password;
+  late final TextEditingController _email;
+  late final TextEditingController _password;
 
-    @override
-    void initState() {
-        _email = TextEditingController();
-        _password = TextEditingController();
-        super.initState();
-    }
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
 
-    @override
-    void dispose() {
-        _email.dispose();
-        _password.dispose();
-        super.dispose();
-    }
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('Login')),
-            body: Column(
-                children: [
-                    TextField(
-                        controller: _email,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(hintText: 'Email'),
-                    ),
-                    TextField(
-                        controller: _password,
-                        obscureText: true,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        decoration: const InputDecoration(hintText: 'Password'),
-                    ),
-                    TextButton(
-                        onPressed: () async {
-                            final email = _email.text;
-                            final password = _password.text;
-                            try {
-                                await FirebaseAuth.instance.signInWithEmailAndPassword(
-                                    email: email,
-                                    password: password,
-                                );
-                                Navigator.of(context).pushNamedAndRemoveUntil(
-                                    notesRoute, 
-                                    (route) => false,
-                                );
-                            } on FirebaseAuthException catch (e) {
-                                if (e.code == 'user-not-found') {
-                                    await showErrorDialog(
-                                        context, 
-                                        'User not found',
-                                    );
-                                } else if (e.code == 'wrong-password') {
-                                    await showErrorDialog(
-                                        context, 
-                                        'Incorrect password',
-                                    );
-                                } else {
-                                    await showErrorDialog(
-                                        context, 
-                                        'Error: ${e.code}',
-                                    );
-                                }
-                            } catch (e) {
-                                await showErrorDialog(
-                                    context, 
-                                    e.toString(),
-                                );
-                            }
-                        },
-                        child: const Text('Login'),
-                    ),
-                    TextButton(
-                        onPressed: () {
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                            registerRoute, 
-                            (route) => false,
-                            );
-                        }, 
-                        child: const Text("Don't have an account? Sign up")
-                    )
-                ],
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: 'Email'),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(hintText: 'Password'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  notesRoute,
+                  (route) => false,
+                );
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'user-not-found') {
+                  await showErrorDialog(
+                    context,
+                    'User not found',
+                  );
+                } else if (e.code == 'wrong-password') {
+                  await showErrorDialog(
+                    context,
+                    'Incorrect password',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
+                }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
+              }
+            },
+            child: const Text('Login'),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  registerRoute,
+                  (route) => false,
+                );
+              },
+              child: const Text("Don't have an account? Sign up"))
+        ],
+      ),
+    );
+  }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,86 +1,108 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
-    const RegisterView({super.key});
-    @override
-    State<RegisterView> createState() => _RegisterViewState();
+  const RegisterView({super.key});
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
 
-    late final TextEditingController _email;
-    late final TextEditingController _password;
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
 
-    @override
-    void initState() {
-        _email = TextEditingController();
-        _password = TextEditingController();
-        super.initState();
-    }
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
-    @override
-    void dispose() {
-        _email.dispose();
-        _password.dispose();
-        super.dispose();
-    }
-
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('Register'),),
-            body: Column(
-                children: [
-                    TextField(
-                        controller: _email,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(hintText: 'Email'),
-                    ),
-                    TextField(
-                        controller: _password,
-                        obscureText: true,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        decoration: const InputDecoration(hintText: 'Password'),
-                    ),
-                    TextButton(
-                        onPressed: () async {
-                            final email = _email.text;
-                            final password = _password.text;
-                            try {
-                                final userCredential =  await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                                email: email,
-                                password: password,
-                            );
-                            devtools.log(userCredential.toString());
-                            } on FirebaseAuthException catch (e) {
-                                if (e.code == 'weak-password') {
-                                    devtools.log('Weak password');
-                                } else if (e.code == 'email-already-in-use') {
-                                    devtools.log('Email is already in use');
-                                } else if (e.code == 'invalid-email') {
-                                    devtools.log('Invalid email');
-                                }
-                            }
-                        },
-                        child: const Text('Register'),
-                    ),
-                    TextButton(onPressed: () {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                        loginRoute, 
-                        (route) => false,
-                        );
-                    },
-                    child: const Text('Already have an account?'),
-                    )
-                ],
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: 'Email'),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(hintText: 'Password'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                Navigator.of(context).pushNamed(verifyEmailRoute);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                  showErrorDialog(
+                    context,
+                    'Weak password',
+                  );
+                } else if (e.code == 'email-already-in-use') {
+                  showErrorDialog(
+                    context,
+                    'Email is already in use',
+                  );
+                } else if (e.code == 'invalid-email') {
+                  showErrorDialog(
+                    context,
+                    'Invalid emai address',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
+                }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
+              }
+            },
+            child: const Text('Register'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                loginRoute,
+                (route) => false,
+              );
+            },
+            child: const Text('Already have an account?'),
+          )
+        ],
+      ),
+    );
+  }
 }
